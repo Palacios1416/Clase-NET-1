1822bb6b2e73c9e487462c1a7576d9d6f0843c00
SISTEMAS2-5 <SISTEMAS2-5@DESKTOP-183TUCH>
2024-01-30 16:01:54 -0600

    termine por hoy

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿int Ndia, Nmes, MDA = 0;
+﻿/*int Ndia, Nmes, MDA = 0;
 Console.WriteLine("CALCULADOR DE DIAS");
 
 Console.WriteLine("Ingrese el dia: ");
@@ -55,5 +55,56 @@ else
             break;
     }
     Console.WriteLine("El " + Ndia + " del " + Nmes + " es el dia " + MDA + " del año");
+}*/
+
+////////////////// segundo ejercicio
+
+Double lado1, lado2, Perimetro=0, Base;
+int Opcion;
+
+Console.WriteLine("TRIANGULOS");
+Console.WriteLine("¿Cual desea resolver?");
+Console.WriteLine("1. Equilatero");
+Console.WriteLine("2. Isoceles");
+Console.WriteLine("3. Rectangulo");
+Console.WriteLine("Ingrese su opcion: ");
+Opcion = int.Parse(Console.ReadLine());
+
+if (Opcion < 1 || Opcion > 3)
+{
+    Console.WriteLine("Opcion incorrecta");
+}
+else
+{
+    switch (Opcion)
+    {
+        case 1:
+            Console.WriteLine("EQUILATERO");
+            Console.WriteLine("Ingrese el tamaño del lado 1");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tamaño del lado 2");
+            lado2 = double.Parse(Console.ReadLine());
+            Perimetro = 3 * lado1;
+            break;
+        case 2:
+            Console.WriteLine("ISOCELES");
+            Console.WriteLine("Ingrese el tamaño del lado 1");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la base");
+            Base = double.Parse(Console.ReadLine());
+            Perimetro = (2 * lado1) + Base;
+            break;
+        case 3:
+            Console.WriteLine("RECTANGULO");
+            Console.WriteLine("Ingrese el tamaño del lado 1");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tamaño del lado 2");
+            lado2 = double.Parse(Console.ReadLine());
+            Perimetro = lado1 + lado2 + (Math.Sqrt((lado1*lado1) + (lado2*lado2)));
+            break;
+        default:
+            break;
+    }
+    Console.WriteLine("El perimetro es: " + Perimetro);
 }
 
