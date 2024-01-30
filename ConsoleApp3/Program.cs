69c57f2e092b6a3944c7c15c0b4115e927e3c711
SISTEMAS2-5 <SISTEMAS2-5@DESKTOP-183TUCH>
2024-01-30 15:38:23 -0600

    trabajito de martes

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,39 +1,59 @@
-﻿
-// cuarto
+﻿int Ndia, Nmes, MDA = 0;
+Console.WriteLine("CALCULADOR DE DIAS");
 
-
-Console.WriteLine("PROGRAMA DE ECUACION CUADRATICA");
-Double A, B, C, dis, x1, x2;
-
-Console.WriteLine("Ingrese el valor de A: ");
-A = Double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el valor de B: ");
-B = Double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el valor de C: ");
-C = Double.Parse(Console.ReadLine());
-
-dis = (Math.Pow(B, 2) - 4 * A * C);
-
-if (A != 0)
+Console.WriteLine("Ingrese el dia: ");
+Ndia = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el mes: ");
+Nmes = int.Parse(Console.ReadLine());
+if ((Ndia < 1 || Ndia > 31) || (Nmes < 1 || Nmes > 12))
 {
-    if (dis < 0)
-    {
-        Console.WriteLine("Tiene raices imaginarias");
-    }
-    else
-    {
-        x1 = (-B + (Math.Sqrt(dis))) / 2 * A;
-        x2 = (-B - (Math.Sqrt(dis))) / 2 * A;
-
-        Console.WriteLine("X1 = " + Math.Round(x1, 3) + " X2 = " + Math.Round(x2, 3));
-        
-    }
+    Console.WriteLine("Los datos introducidos son incorrectos");
 }
 else
 {
-    Console.WriteLine("El coeficiente cuadratico debe ser diferente de cero");
+    switch (Nmes)
+    {
+        case 1:
+            MDA = Ndia;
+            break;
+        case 2:
+            MDA = Ndia + 31;
+            break;
+        case 3:
+            MDA = Ndia + 59;
+            break;
+        case 4:
+            MDA = Ndia + 90;
+            break;
+        case 5:
+            MDA = Ndia + 120;
+            break;
+        case 6:
+            MDA = Ndia + 151;
+            break;
+        case 7:
+            MDA = Ndia + 181;
+            break;
+        case 8:
+            MDA = Ndia + 212;
+            break;
+        case 9:
+            MDA = Ndia + 243;
+            break;
+        case 10:
+            MDA = Ndia + 273;
+            break;
+        case 11:
+            MDA = Ndia + 304;
+            break;
+        case 12:
+            MDA = Ndia + 334;
+            break;
+
+        default:
+
+            break;
+    }
+    Console.WriteLine("El " + Ndia + " del " + Nmes + " es el dia " + MDA + " del año");
 }
-
-
-
 
